47e5b14419926cf4f1b2fd514a899f6f0e3eef47
Brian Goad <bdgoad@gmail.com>
2016-08-31 12:25:40 -0400

    Updates

--- make_links.sh
+++ make_links.sh
@@ -2,7 +2,7 @@
 
 DIRNAME=$(cd $(dirname $0) && pwd)
 
-for dfile in $(ls -d1 .[^.]* | grep -v git); do
+for dfile in $(ls -d1 .[^.]* | grep -v '^.git$'); do
 
 	if [ -e ~/${dfile} ]; then
 		echo "Backing up ~/${dfile}"
